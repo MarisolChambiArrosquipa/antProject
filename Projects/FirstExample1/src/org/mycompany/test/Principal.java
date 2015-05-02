b4e7f7595c3e5791d4eb77b13a28254191b12a62
Marisol Chambi Arrosquipa <marysol_m5@hotmail.com>
2015-05-02 15:41:34 -0400

    modificando

--- Projects/FirstExample1/src/org/mycompany/test/Principal.java
+++ Projects/FirstExample1/src/org/mycompany/test/Principal.java
@@ -3,7 +3,7 @@ package org.mycompany.test;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("run the example with Ant");
+		System.out.println("run the example with Ant wordddddddd");
 
 	}
 
